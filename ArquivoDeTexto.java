8afa43726edefd033b0c8e91cc7e393f8e1489f0
JOSÉ WHENDEL MACHADO DE SOUSA LIMA <jose.lima@alunos.sc.senac.br>
2019-10-07 19:30:39 -0300

    Leitura de arquivo TXT

---
+++ ArquivoDeTexto.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ArquivoDeTexto {
+    public static void main(String[] args) {
+        try{
+            BufferedReader leitura = new BufferedReader(new FileReader("arquivo-desordenado.txt"));
+            String value = "";
+            while (true){
+                if(value != null)
+                    System.out.println(value);
+                else
+                    break;
+                
+                value = leitura.readLine();
+
+            }
+            leitura.close();
+        } catch (IOException exception){
+            System.out.println("Erro de exceção I/O: " + exception.getMessage());
+        }
+        
+    }
+}
